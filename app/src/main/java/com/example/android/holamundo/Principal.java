01d8f13dbac6eecc4c7ac470a6762e1c28eec9b4
Marlon Guerra <mguerramulford@gmail.com>
2017-05-06 11:11:49 -0500

    funciones terminadas

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -2,12 +2,62 @@ package com.example.android.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+
+    private EditText cajanumero1;
+    private EditText cajanumero2;
+    private TextView cajaresultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        cajanumero1 = (EditText)findViewById(R.id.txtnumero1);
+        cajanumero2 = (EditText)findViewById(R.id.txtnumero2);
+        cajaresultado =(TextView)findViewById(R.id.txtresultado);
+    }
+
+    public void Calcular(View v)
+    {
+        if(validar())
+        {
+            double n1, n2, suma;
+            n1 = Integer.parseInt(cajanumero1.getText().toString());
+            n2 = Integer.parseInt(cajanumero2.getText().toString());
+            suma = n1 + n2;
+            cajaresultado.setText(""+suma);
+        }
+
+
+    }
+
+    public void Borrar(View v)
+    {
+        cajanumero1.setText("");
+        cajanumero2.setText("");
+        cajaresultado.setText("");
+        cajanumero1.requestFocus();
+    }
+
+    public boolean validar()
+    {
+        if(cajanumero1.getText().toString().isEmpty())
+        {
+            cajanumero1.setError(this.getResources().getString(R.string.error_numero1));
+            cajanumero1.requestFocus();
+            return false;
+        }
+        else if(cajanumero2.getText().toString().isEmpty())
+        {
+            cajanumero2.setError(this.getResources().getString(R.string.error_numero2));
+            cajanumero2.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
